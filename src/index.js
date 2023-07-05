b3c853568bb962ca8cad6f6e547f60ba6b7579f4
Dolnys <thedolnys@gmail.com>
2023-07-05 21:48:13 +0200

    Improving  star component Reusability

--- src/index.js
+++ src/index.js
@@ -8,5 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
+    <StarRating
+      maxRating={3}
+      size={25}
+      messages={['ok', 'good', 'cool']}
+      defaultRating={1}
+    />
   </React.StrictMode>
 )
