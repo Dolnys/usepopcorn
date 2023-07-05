c21835fda020e5b98533632db212317469b3a67d
Dolnys <thedolnys@gmail.com>
2023-07-05 20:46:46 +0200

    Add hover state for the stars

--- src/StarRating.js
+++ src/StarRating.js
@@ -22,8 +22,9 @@ const starStyle = {
   cursor: 'pointer',
 }
 
-function StarRating({ maxRating = 1 }) {
+function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0)
+  const [tempRating, setTempRating] = useState(0)
 
   function handleRating(rating) {
     setRating(rating)
@@ -36,18 +37,25 @@ function StarRating({ maxRating = 1 }) {
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ''}</p>
+      <p style={textStyle}>{tempRating || rating || ''}</p>
     </div>
   )
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span style={starStyle} onClick={onRate}>
+    <span
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns='http://www.w3.org/2000/svg'
